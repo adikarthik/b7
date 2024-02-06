f57360b390f2791b30b13c5db3702f1741352d9a
adikarthik <33541222+adikarthik@users.noreply.github.com>
2024-02-06 20:41:58 +0530

    Update file.tf

--- file.tf
+++ file.tf
@@ -4,7 +4,12 @@ resource "local_file" "abc_number" {
 
 
 }
+resource "local_file" "abc_number1" {
+  filename = "abcd.txt"
+  content  = "abd"
 
+
+}
 output test {
  value=local_file.abc_number.id
 }
